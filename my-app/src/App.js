7482340c0a8ec16e8e620c26f6bb82125bcf6ac3
Leonid <leonid.bgh@gmail.com>
2022-10-29 06:37:09 +0500

    add fucntion greeting and indentif user

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,28 @@
+let user = {
+  firstName: 'Ivan',
+  lastName: 'Petrovich'
+}
+//create object user
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+//return user firstname and lastName
+
+function getGreetings(user){
+  if(user) {
+    return <h1>Hello, {formatName(user)}</h1>
+  }
+  else {
+    return <h1>Hello, misterious</h1>
+  }
+}
+//operator else if on jsx
 
 function App() {
   return (
     <div className="App">
+      <h1>Hello, {getGreetings(user)}</h1> {/*fucntion in {fucntion then (object)} */}
     </div>
   );
 }
