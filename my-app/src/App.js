93beb74bfe956330727bbb9d93f9186455430d52
Leonid <leonid.bgh@gmail.com>
2022-10-29 07:41:12 +0500

    clear project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,7 @@
-import ToDoList from './components/ToDoList';
-let user = {
-  firstName: 'Ivan',
-  lastName: 'Petrovich'
-}
-//create object user
-
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
-//return user firstname and lastName
-
-function getGreetings(user) {
-  if (user) {
-    return <h1>Hello, {formatName(user)}</h1>
-  }
-  else {
-    return <h1>Hello, misterious</h1>
-  }
-}
-//operator else if on jsx
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>
-}
-
 function App() {
   return (
     <div className="App">
-      <h1 className="greet">Hello, {getGreetings(user)}</h1> {/*fucntion in {fucntion then (object)} */}
-      <a target="_blank" href="https://google.com"> link</a>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
-      <Welcome name='Alice' />
-      <ToDoList />
+
     </div>
   );
 }
